69050136c705957eef4ae3f2a8f451978b9d2030
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2024-09-19 13:55:38 -0700

    Fixed depencies
    - removed hono fully
    - moved backend depencies to backend
    - fixed dev depencies being in regular depencies

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,14 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3000
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '')
+      }
+    }
   },
   plugins: [react(), tailwindcss()],
 })
