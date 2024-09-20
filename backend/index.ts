f53e2a3b33a33edde404b802f28616543d84b020
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2024-09-19 23:23:43 -0700

    Replaced express with hono
    When using hono for the first time I ran into the problem of feeling overwhelmed by understanding using hono, bun as well as connecting my frontend and backend. I used express to minimize the amount of new things I was trying to implement all at once then went back to hono since it's more performance orieted. Not that this small app will need it...

--- backend/index.ts
+++ backend/index.ts
@@ -1,26 +1,28 @@
-import express from 'express'
-import cors from 'cors'
-const app = express()
+import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+const app = new Hono()
 
 // app.use('/static/*', serveStatic({ root: './' }))
 // ^ how you'd go about serving static files in a folder named 'static'
 const port = process.env.PORT || 8080
 app.use(cors())
-app.use(express.json());
-app.post('/api', (req, res) => {
+app.post('/api', async (c) => {
   console.log('Request from frontend on backend print...')
-  const { amount_to_convert, unit_convert_from, unit_convert_to } = req.body
+  const { amount_to_convert, unit_convert_from, unit_convert_to } = await c.req.json()
   const data = {
     amount_to_convert: amount_to_convert,
     unit_convert_from: unit_convert_from,
     unit_convert_to: unit_convert_to
   }
   console.log(data)
-  res.json({ ...data })
+  return c.json({ ...data })
 })
-app.listen(port, () => {
-  console.log(`Listening on port: ${port}`)
+Bun.serve({
+  port: port,
+  fetch: app.fetch
 })
+
+console.log(`Running on port ${port}`)
 
 
 
