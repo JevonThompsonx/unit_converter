0aace22e1c82afd16b32de1b319fafb061cdb3b8
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2024-09-19 23:05:10 -0700

    Created base for handling data from req.body
    1. grabs values from body
    2. turns it into an object
    3. sends object back as json

--- backend/index.ts
+++ backend/index.ts
@@ -8,9 +8,15 @@ const port = process.env.PORT || 8080
 app.use(cors())
 app.use(express.json());
 app.post('/api', (req, res) => {
-  console.log('Request fromt frontend on backend print...')
-  console.log(req.body)
-  res.json({ hello: 'hi' })
+  console.log('Request from frontend on backend print...')
+  const { amount_to_convert, unit_convert_from, unit_convert_to } = req.body
+  const data = {
+    amount_to_convert: amount_to_convert,
+    unit_convert_from: unit_convert_from,
+    unit_convert_to: unit_convert_to
+  }
+  console.log(data)
+  res.json({ ...data })
 })
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`)
