2ef1a834d9e7332f9ef2175c50737235bf108df0
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2024-09-21 02:52:49 -0400

    Added base path to make routing easier

--- backend/index.ts
+++ backend/index.ts
@@ -1,12 +1,12 @@
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
-const app = new Hono()
+const app = new Hono().basePath("/api"
 
 // app.use('/static/*', serveStatic({ root: './' }))
 // ^ how you'd go about serving static files in a folder named 'static'
 const port = process.env.PORT || 8080
 app.use(cors())
-app.post('/api', async (c) => {
+app.post('/', async (c) => {
   console.log('Request from frontend on backend print...')
   const { amount_to_convert, unit_convert_from, unit_convert_to } = await c.req.json()
   const data = {
