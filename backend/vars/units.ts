6eeb0499256426a336eb22dc84518de41168f1fd
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2024-09-22 19:50:21 -0700

    Created units to store and export units
    An object of available units and their symbols

---
+++ backend/vars/units.ts
@@ -0,0 +1,26 @@
+const units = {
+  length: {
+    Millimeter: 'mm',
+    Centimeter: 'cm',
+    Meter: 'm',
+    Kilometer: 'km',
+    Inch: 'in',
+    Foot: 'ft',
+    Yard: 'yd',
+    Mile: 'mi'
+  },
+  weight: {
+    Grams: 'g',
+    Milligrams: 'mg',
+    Pounds: 'lb',
+    Ounce: 'oz',
+    Killograms: 'kg'
+  },
+  temperature: {
+    Celsius: 'C',
+    Fahrenheit: "F",
+    Kelvin: "K"
+  }
+}
+
+export default units 
