fd86821f86ab70f0a15973931c6dc6e410069563
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2024-09-25 09:17:17 -0700

    Create AmountInput.tsx
    Added props

---
+++ frontend/src/components/AmountInput.tsx
@@ -0,0 +1,15 @@
+
+export default ({amount_to_convert,ValidSpan, InValidSpan,checkValidity})=> {
+return (<label htmlFor="amount_to_convert">
+              Amount to convert
+            </label>
+            <div className="flex flex-row justify-evenly space-x-2 items-center">
+              <input type="number" id="amount_to_convert" name="amount_to_convert" className="p-2 invalid:text-red-500 focus:border-blue-500 valid:text-green-600" min={1} placeholder="Amount to convert" required onChange={checkValidity} onBlur={checkValidity} ref={amount_to_convert} >
+
+              </input>
+              {
+                validity.amount ?
+                  <ValidSpan /> :
+                  <InValidSpan />
+              }
+            </div>)}
