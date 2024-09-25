1de0f79305bf333fed899e1309a514af5e1aae0d
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2024-09-24 21:08:06 -0700

    Create fromSelect.tsx
    Will need to pass state as prop and find rest of variables to split up successfully

---
+++ frontend/src/fromSelect.tsx
@@ -0,0 +1,22 @@
+import {useState} from "react" 
+import {ValidSpan, InValidSpan} from "../components/ValiditySpans"
+
+export default ()=> {
+  const [toValue, setToValue] = useState('')
+  <>
+    <label htmlFor="unit_convert_from">
+              Unit to convert from
+            </label>
+
+            <div className="flex flex-row justify-evenly space-x-2 items-center">
+              <select className="p-2 invalid:text-red-500 focus:border-blue-500 valid:text-green-600" id="unit_convert_from" name="unit_convert_from" required value={fromValue} onBlur={checkValidity} onChange={handleFromValue} ref={unit_convert_from}>
+                <UnitsComponent />
+              </select>
+              {
+                validity.from ?
+                  <ValidSpan /> :
+                  <InValidSpan />
+              }
+            </div>
+            </> 
+} 
