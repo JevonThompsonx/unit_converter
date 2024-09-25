46a081c497f1f2c1d2cebd5e7160eecdf6a3e097
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2024-09-25 17:04:42 -0400

    Updated fixed with unitTypes not functioning correctly
    `unitTypes` wasn't being properly spread which caused a crash

--- frontend/src/components/FromSelect.tsx
+++ frontend/src/components/FromSelect.tsx
@@ -7,7 +7,7 @@ export default ({ ValidSpan, InValidSpan, UnitsComponent, fromValue, handleFromV
 
       <div className="flex flex-row justify-evenly space-x-2 items-center">
         <select className="p-2 invalid:text-red-500 focus:border-blue-500 valid:text-green-600" id="unit_convert_from" name="unit_convert_from" required value={fromValue} onBlur={checkValidity} onChange={handleFromValue} ref={unit_convert_from}>
-          <UnitsComponent units={{ ...units }} unitTypes={{ unitTypes }} />
+          <UnitsComponent units={{ ...units }} unitTypes={{ ...unitTypes }} />
 
         </select>
         {
--- frontend/src/components/ToSelect.tsx
+++ frontend/src/components/ToSelect.tsx
@@ -7,7 +7,7 @@ export default ({ ValidSpan, InValidSpan, UnitsComponent, toValue, handleToValue
 
       <div className="flex flex-row justify-evenly space-x-2 items-center">
         <select className="p-2 invalid:text-red-500 focus:border-blue-500 valid:text-green-600" id="unit_convert_to" name="unit_convert_to" required value={toValue} onBlur={checkValidity} onChange={handleToValue} ref={unit_convert_to}>
-          <UnitsComponent units={{ ...units }} unitTypes={{ unitTypes }} />
+          <UnitsComponent units={{ ...units }} unitTypes={{ ...unitTypes }} />
         </select>
         {
           validity.to ?
