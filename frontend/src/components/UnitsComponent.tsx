f5c996bc93dc7df3040106dc83767659b3597ea6
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2024-09-25 16:59:04 -0400

    renamed UnitsComponent from UnitsComponents

---
+++ frontend/src/components/UnitsComponent.tsx
@@ -0,0 +1,29 @@
+export default ({ units, unitTypes }) => {
+  return (
+    <>
+      if (unitTypes.length.active) {
+        <>
+          <option value="" >--please choose an option--</option>
+          {Object.entries(units.length).map((item) => {
+            return <option value={item[1]} key={item[0]}>{item[0]}</option>
+          })}
+        </>
+      }
+      if (unitTypes.weight.active) {
+        <>
+          <option value="" >--please choose an option--</option>
+          {Object.entries(units.weight).map((item) => {
+            return <option value={item[1]} key={item[0]}>{item[0]}</option>
+          })}
+        </>
+      }
+      if (unitTypes.temperature.active) {
+        <>
+          <option value="" >--please choose an option--</option>
+          {Object.entries(units.temperature).map((item) => {
+            return <option value={item[1]} key={item[0]}>{item[0]}</option>
+          })}
+        </>
+      }
+    </>)
+}
