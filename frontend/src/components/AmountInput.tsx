83ee80345205e461f4c517fd56361b2f39ccf3a7
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2024-09-26 20:06:09 -0700

    Update AmountInput.tsx w/ validity

--- frontend/src/components/AmountInput.tsx
+++ frontend/src/components/AmountInput.tsx
@@ -6,7 +6,8 @@ const AmountInput = ({ amount_to_convert, ValidSpan, InValidSpan, checkValidity,
         Amount to convert
       </label>
       <div className="flex flex-row justify-evenly space-x-2 items-center">
-        <input type="number" id="amount_to_convert" name="amount_to_convert" className="p-2 invalid:text-red-500 focus:border-blue-500 valid:text-green-600" min={1} placeholder="Amount to convert" required onChange={checkValidity} onBlur={checkValidity} ref={amount_to_convert} >
+        <input type="number" id="amount_to_convert" name="amount_to_convert" className="p-2 invalid:text-red-500 focus:border-blue-500 valid:text-green-600" min={1} placeholder="Amount to convert" required onChange={()=>checkValidity({ amount_to_convert, unit_convert_to, unit_convert_from, setValidity, validity} onBlur={()=> checkValidity({ amount_to_convert, unit_convert_to, unit_convert_from, setValidity, validity
+} ref={amount_to_convert} >
 
         </input>
         {
