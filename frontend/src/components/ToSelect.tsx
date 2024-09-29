365b0d11a3bcbc21a4b43e59d4e72a27eb8a634b
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2024-09-29 00:08:35 -0700

    Update ToSelect.tsx w/ replacing handleToValue to a function on the on change instead

--- frontend/src/components/ToSelect.tsx
+++ frontend/src/components/ToSelect.tsx
@@ -1,7 +1,7 @@
 import { useContext } from "react"
 import { Context } from "../pages/Home"
 const ToSelect = () => {
-  const { ValidSpan, InValidSpan, UnitsComponent, toValue, handleToValue, unit_convert_to, validity, units, unitTypes } = useContext(Context)
+  const { ValidSpan, InValidSpan, UnitsComponent, toValue, unit_convert_to, validity, units, unitTypes } = useContext(Context)
 
 
   return (
@@ -11,7 +11,7 @@ const ToSelect = () => {
       </label>
 
       <div className="flex flex-row justify-evenly space-x-2 items-center">
-        <select className="p-2 invalid:text-red-500 focus:border-blue-500 valid:text-green-600" id="unit_convert_to" name="unit_convert_to" required value={toValue} onChange={handleToValue} ref={unit_convert_to}>
+        <select className="p-2 invalid:text-red-500 focus:border-blue-500 valid:text-green-600" id="unit_convert_to" name="unit_convert_to" required value={toValue} onChange={(e: React.ChangeEvent<HTMLSelectElement>) => setToValue(e.target.value)} ref={unit_convert_to}>
           <UnitsComponent units={{ ...units }} unitTypes={{ ...unitTypes }} />
 
         </select>
