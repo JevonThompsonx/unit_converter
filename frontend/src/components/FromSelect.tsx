4c0dbb1ecb221f234f7ade204fbaca31783a1bd9
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2024-09-29 00:11:35 -0700

    Update FromSelect.tsx w/ adding handle from as arrow function on the on change

--- frontend/src/components/FromSelect.tsx
+++ frontend/src/components/FromSelect.tsx
@@ -1,7 +1,7 @@
 import { useContext } from "react"
 import { Context } from "../pages/Home"
 const FromSelect = () => {
-  const { ValidSpan, InValidSpan, UnitsComponent, fromValue, handleFromValue, unit_convert_from, validity, units, unitTypes } = useContext(Context)
+  const { ValidSpan, InValidSpan, UnitsComponent, fromValue, unit_convert_from, validity, units, unitTypes } = useContext(Context)
 
 
   return (
@@ -12,7 +12,7 @@ const FromSelect = () => {
 
       <div className="flex flex-row justify-evenly space-x-2 items-center">
         <select className="p-2 invalid:text-red-500 focus:border-blue-500 valid:text-green-600" id="unit_convert_from" name="unit_convert_from" required value={fromValue
-        } onChange={handleFromValue} ref={unit_convert_from}>
+        } onChange={(e: React.ChangeEvent<HTMLSelectElement>) => setFromValue(e.target.value)} ref={unit_convert_from}>
           <UnitsComponent units={{ ...units }} unitTypes={{ ...unitTypes }} />
 
         </select>
